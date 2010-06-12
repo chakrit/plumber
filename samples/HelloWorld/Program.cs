06e633267b823594bcbabd0927f6d2d3a568f4a8
Chakrit Wichian <chakrit@2nitedesign.com>
2010-06-12 23:25:38 +0700

    Initial solution creation

---
+++ samples/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+  public class Program
+  {
+    internal static void Main() { new Program().Run(); }
+
+
+    public void Run()
+    {
+      Console.WriteLine("HELLO WORLD!");
+    }
+  }
+}
---
+++ src/PlumberAssemblyInfo.cs
@@ -0,0 +1,16 @@
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Plumber")]
+[assembly: AssemblyDescription("Embedded web server implementation in C#")]
+[assembly: AssemblyCompany("2nitedesign! Co., Ltd.")]
+[assembly: AssemblyProduct("Plumber")]
+[assembly: AssemblyCopyright("Copyright © 2010 2nitedesign! Co., Ltd. Licensed under the New BSD License.")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("71ce8aa6-5798-4a23-86b0-ac9271ae98e1")]
+
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.*")]
